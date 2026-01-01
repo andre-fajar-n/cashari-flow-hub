430e77753cfe41b7075ad9337ec44ea17ea1c84c
andre-fajar-n <andrenugroho395@gmail.com>
2026-01-01 12:26:41 +0700

    feat: Format currency amounts with a minimum of 4 fractional digits.

--- src/lib/currency.ts
+++ src/lib/currency.ts
@@ -1,5 +1,5 @@
 export const formatAmountCurrency = (amount: number, currencyCode: string = 'unknown', currencySymbol: string = 'unknown') => {
   const isNegative = amount < 0;
   const showSymbol = currencySymbol !== currencyCode;
-  return `${isNegative ? '-' : ''}${currencyCode} ${Math.abs(amount).toLocaleString('id-ID')}${showSymbol ? ` ${currencySymbol}` : ''}`;
+  return `${isNegative ? '-' : ''}${currencyCode} ${Math.abs(amount).toLocaleString('id-ID', { minimumFractionDigits: 4 })}${showSymbol ? ` ${currencySymbol}` : ''}`;
 };
