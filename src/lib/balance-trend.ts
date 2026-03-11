b8604f414d50bb4145dde698c4783e23ad96aac7
andre-fajar-n <andrenugroho395@gmail.com>
2026-03-11 07:26:46 +0700

    fix: Adjust valuation status conditions to check for non-zero cumulative amounts and units.

--- src/lib/balance-trend.ts
+++ src/lib/balance-trend.ts
@@ -6,16 +6,16 @@ export const getStatus = (item: DailyCumulative, date: string) => {
   let status: ValuationStatus = 'Exact';
   const FXRateDateNotSameAsDate = item.fx_rate_date_used !== date &&
     item.original_currency_code !== item.base_currency_code &&
-    item.cumulative_amount > 0;
+    item.cumulative_amount !== 0;
   const AssetPriceDateNotSameAsDate = item.asset_price_date_used !== date &&
     item.is_trackable &&
-    item.cumulative_unit > 0;
+    item.cumulative_unit !== 0;
   const isMissingFX = !item.historical_fx_rate &&
     item.original_currency_code !== item.base_currency_code &&
-    item.cumulative_amount > 0;
+    item.cumulative_amount !== 0;
   const isMissingPrice = !item.historical_asset_price &&
     item.is_trackable &&
-    item.cumulative_unit > 0;
+    item.cumulative_unit !== 0;
 
   if (isMissingPrice && isMissingFX) {
     status = 'Missing';
